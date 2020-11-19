591098a44cd6439a09eba0364f91663b962a4e20
Opilane TTHK <opilane@tthk.ee>
2020-11-19 09:36:54 +0200

    Добавление Form2 кнопки и пикчербокс

--- redactor_valjataga/Form2.Designer.cs
+++ redactor_valjataga/Form2.Designer.cs
@@ -31,165 +31,196 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.hscrollBar_red = new System.Windows.Forms.HScrollBar();
-            this.hscrollBar_green = new System.Windows.Forms.HScrollBar();
-            this.hscrollBar_blue = new System.Windows.Forms.HScrollBar();
-            this.numericUpDown_red = new System.Windows.Forms.NumericUpDown();
-            this.numericUpDown_green = new System.Windows.Forms.NumericUpDown();
-            this.numericUpDown_blue = new System.Windows.Forms.NumericUpDown();
+            this.Scroll_Red = new System.Windows.Forms.HScrollBar();
+            this.Scroll_Green = new System.Windows.Forms.HScrollBar();
+            this.numeric_Red = new System.Windows.Forms.NumericUpDown();
+            this.numeric_Green = new System.Windows.Forms.NumericUpDown();
+            this.Scroll_Blue = new System.Windows.Forms.HScrollBar();
+            this.numeric_Blue = new System.Windows.Forms.NumericUpDown();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.buttonOther = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_red)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_green)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_blue)).BeginInit();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_Red)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_Green)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_Blue)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 44);
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(27, 77);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(27, 13);
-            this.label1.TabIndex = 0;
+            this.label1.TabIndex = 11;
             this.label1.Text = "Red";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(43, 118);
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(27, 134);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(36, 13);
-            this.label2.TabIndex = 1;
+            this.label2.TabIndex = 12;
             this.label2.Text = "Green";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(51, 185);
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Location = new System.Drawing.Point(27, 216);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(28, 13);
-            this.label3.TabIndex = 2;
+            this.label3.TabIndex = 13;
             this.label3.Text = "Blue";
             // 
-            // hscrollBar_red
+            // Scroll_Red
             // 
-            this.hscrollBar_red.Location = new System.Drawing.Point(124, 32);
-            this.hscrollBar_red.Name = "hscrollBar_red";
-            this.hscrollBar_red.Size = new System.Drawing.Size(172, 25);
-            this.hscrollBar_red.TabIndex = 3;
-            this.hscrollBar_red.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hscrollBar_red_Scroll);
+            this.Scroll_Red.LargeChange = 1;
+            this.Scroll_Red.Location = new System.Drawing.Point(89, 77);
+            this.Scroll_Red.Maximum = 255;
+            this.Scroll_Red.Name = "Scroll_Red";
+            this.Scroll_Red.Size = new System.Drawing.Size(226, 25);
+            this.Scroll_Red.TabIndex = 14;
+            this.Scroll_Red.Scroll += new System.Windows.Forms.ScrollEventHandler(this.Scroll_Red_ValueChanged);
             // 
-            // hscrollBar_green
+            // Scroll_Green
             // 
-            this.hscrollBar_green.Location = new System.Drawing.Point(124, 106);
-            this.hscrollBar_green.Name = "hscrollBar_green";
-            this.hscrollBar_green.Size = new System.Drawing.Size(172, 25);
-            this.hscrollBar_green.TabIndex = 4;
-            this.hscrollBar_green.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hscrollBar_green_Scroll);
+            this.Scroll_Green.LargeChange = 1;
+            this.Scroll_Green.Location = new System.Drawing.Point(89, 134);
+            this.Scroll_Green.Maximum = 255;
+            this.Scroll_Green.Name = "Scroll_Green";
+            this.Scroll_Green.Size = new System.Drawing.Size(226, 25);
+            this.Scroll_Green.TabIndex = 15;
+            this.Scroll_Green.Scroll += new System.Windows.Forms.ScrollEventHandler(this.Scroll_Green_ValueChanged);
             // 
-            // hscrollBar_blue
+            // numeric_Red
             // 
-            this.hscrollBar_blue.Location = new System.Drawing.Point(124, 173);
-            this.hscrollBar_blue.Name = "hscrollBar_blue";
-            this.hscrollBar_blue.Size = new System.Drawing.Size(172, 25);
-            this.hscrollBar_blue.TabIndex = 5;
-            this.hscrollBar_blue.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hscrollBar_blue_Scroll);
+            this.numeric_Red.Location = new System.Drawing.Point(348, 82);
+            this.numeric_Red.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numeric_Red.Name = "numeric_Red";
+            this.numeric_Red.Size = new System.Drawing.Size(72, 20);
+            this.numeric_Red.TabIndex = 16;
+            this.numeric_Red.ValueChanged += new System.EventHandler(this.Numeric_Red_ValueChanged);
             // 
-            // numericUpDown_red
+            // numeric_Green
             // 
-            this.numericUpDown_red.Location = new System.Drawing.Point(364, 44);
-            this.numericUpDown_red.Name = "numericUpDown_red";
-            this.numericUpDown_red.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown_red.TabIndex = 6;
-            this.numericUpDown_red.ValueChanged += new System.EventHandler(this.numericUpDown_red_ValueChanged);
+            this.numeric_Green.Location = new System.Drawing.Point(348, 139);
+            this.numeric_Green.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numeric_Green.Name = "numeric_Green";
+            this.numeric_Green.Size = new System.Drawing.Size(73, 20);
+            this.numeric_Green.TabIndex = 17;
+            this.numeric_Green.ValueChanged += new System.EventHandler(this.Numeric_Green_ValueChanged);
             // 
-            // numericUpDown_green
+            // Scroll_Blue
             // 
-            this.numericUpDown_green.Location = new System.Drawing.Point(364, 118);
-            this.numericUpDown_green.Name = "numericUpDown_green";
-            this.numericUpDown_green.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown_green.TabIndex = 7;
-            this.numericUpDown_green.ValueChanged += new System.EventHandler(this.numericUpDown_green_ValueChanged);
+            this.Scroll_Blue.LargeChange = 1;
+            this.Scroll_Blue.Location = new System.Drawing.Point(89, 204);
+            this.Scroll_Blue.Maximum = 255;
+            this.Scroll_Blue.Name = "Scroll_Blue";
+            this.Scroll_Blue.Size = new System.Drawing.Size(226, 25);
+            this.Scroll_Blue.TabIndex = 18;
+            this.Scroll_Blue.Scroll += new System.Windows.Forms.ScrollEventHandler(this.Scroll_Blue_ValueChanged);
             // 
-            // numericUpDown_blue
+            // numeric_Blue
             // 
-            this.numericUpDown_blue.Location = new System.Drawing.Point(364, 178);
-            this.numericUpDown_blue.Name = "numericUpDown_blue";
-            this.numericUpDown_blue.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown_blue.TabIndex = 8;
-            this.numericUpDown_blue.ValueChanged += new System.EventHandler(this.numericUpDown_blue_ValueChanged);
+            this.numeric_Blue.Location = new System.Drawing.Point(348, 209);
+            this.numeric_Blue.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numeric_Blue.Name = "numeric_Blue";
+            this.numeric_Blue.Size = new System.Drawing.Size(72, 20);
+            this.numeric_Blue.TabIndex = 19;
+            this.numeric_Blue.ValueChanged += new System.EventHandler(this.Numeric_Blue_ValueChanged);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(565, 12);
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(439, 114);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(190, 186);
-            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.Size = new System.Drawing.Size(153, 142);
+            this.pictureBox1.TabIndex = 20;
             this.pictureBox1.TabStop = false;
-            // 
-            // buttonOther
-            // 
-            this.buttonOther.BackColor = System.Drawing.Color.RosyBrown;
-            this.buttonOther.Font = new System.Drawing.Font("Arial Narrow", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.buttonOther.Location = new System.Drawing.Point(614, 212);
-            this.buttonOther.Name = "buttonOther";
-            this.buttonOther.Size = new System.Drawing.Size(97, 51);
-            this.buttonOther.TabIndex = 10;
-            this.buttonOther.Text = "другой цвет";
-            this.buttonOther.UseVisualStyleBackColor = false;
-            this.buttonOther.Click += new System.EventHandler(this.buttonOther_Click);
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.Olive;
-            this.button1.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.button1.Location = new System.Drawing.Point(46, 239);
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(30, 289);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(120, 33);
-            this.button1.TabIndex = 11;
+            this.button1.Size = new System.Drawing.Size(106, 50);
+            this.button1.TabIndex = 21;
             this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.Color.Olive;
-            this.button2.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.button2.Location = new System.Drawing.Point(360, 239);
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(193, 289);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(124, 33);
-            this.button2.TabIndex = 12;
+            this.button2.Size = new System.Drawing.Size(106, 50);
+            this.button2.TabIndex = 22;
             this.button2.Text = "Cancel";
             this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.ForeColor = System.Drawing.Color.Transparent;
+            this.button3.Location = new System.Drawing.Point(469, 288);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 51);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "Другой цвет";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::redactor_valjataga.Properties.Resources.PL71482_12;
-            this.ClientSize = new System.Drawing.Size(800, 275);
+            this.ClientSize = new System.Drawing.Size(604, 372);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.buttonOther);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.numericUpDown_blue);
-            this.Controls.Add(this.numericUpDown_green);
-            this.Controls.Add(this.numericUpDown_red);
-            this.Controls.Add(this.hscrollBar_blue);
-            this.Controls.Add(this.hscrollBar_green);
-            this.Controls.Add(this.hscrollBar_red);
+            this.Controls.Add(this.numeric_Blue);
+            this.Controls.Add(this.Scroll_Blue);
+            this.Controls.Add(this.numeric_Green);
+            this.Controls.Add(this.numeric_Red);
+            this.Controls.Add(this.Scroll_Green);
+            this.Controls.Add(this.Scroll_Red);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form2";
             this.Text = "Colors";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_red)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_green)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_blue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_Red)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_Green)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_Blue)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -201,15 +232,15 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.HScrollBar hscrollBar_red;
-        private System.Windows.Forms.HScrollBar hscrollBar_green;
-        private System.Windows.Forms.HScrollBar hscrollBar_blue;
-        private System.Windows.Forms.NumericUpDown numericUpDown_red;
-        private System.Windows.Forms.NumericUpDown numericUpDown_green;
-        private System.Windows.Forms.NumericUpDown numericUpDown_blue;
+        private System.Windows.Forms.HScrollBar Scroll_Red;
+        private System.Windows.Forms.HScrollBar Scroll_Green;
+        private System.Windows.Forms.NumericUpDown numeric_Red;
+        private System.Windows.Forms.NumericUpDown numeric_Green;
+        private System.Windows.Forms.HScrollBar Scroll_Blue;
+        private System.Windows.Forms.NumericUpDown numeric_Blue;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button buttonOther;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- redactor_valjataga/Form2.cs
+++ redactor_valjataga/Form2.cs
@@ -6,28 +6,26 @@ namespace redactor_valjataga
 {
     public partial class Form2 : Form
     {
-        public Color colorResult;
+        Color colorResult;
         Color color;
         public Form2()
         {
             InitializeComponent();
+            Scroll_Red.Tag = numeric_Red;
+            Scroll_Green.Tag = numeric_Green;
+            Scroll_Blue.Tag = numeric_Blue;
 
-            hscrollBar_red.Tag = numericUpDown_red;
-            hscrollBar_green.Tag = numericUpDown_green;
-            hscrollBar_blue.Tag = numericUpDown_blue;
+            numeric_Red.Tag = Scroll_Red;
+            numeric_Green.Tag = Scroll_Green;
+            numeric_Blue.Tag = Scroll_Blue;
 
+            numeric_Red.Value = color.R;
+            numeric_Green.Value = color.G;
+            numeric_Blue.Value = color.B;
 
-            numericUpDown_red.Tag = hscrollBar_red;
-            numericUpDown_green.Tag = hscrollBar_green;
-            numericUpDown_blue.Tag = hscrollBar_blue;
-
-
-            numericUpDown_red.Value = color.R;
-            numericUpDown_green.Value = color.G;
-            numericUpDown_blue.Value = color.B;
         }
 
-        private void hscrollBar_red_Scroll(object sender, ScrollEventArgs e)
+        private void Scroll_Red_ValueChanged(object sender, ScrollEventArgs e)
         {
             ScrollBar scrollBar = (ScrollBar)sender;
             NumericUpDown numericUpDown = (NumericUpDown)scrollBar.Tag;
@@ -35,77 +33,74 @@ namespace redactor_valjataga
             UpdateColor();
         }
 
+        private void Scroll_Green_ValueChanged(object sender, ScrollEventArgs e)
+        {
+            ScrollBar scrollBar = (ScrollBar)sender;
+            NumericUpDown numericUpDown = (NumericUpDown)scrollBar.Tag;
+            numericUpDown.Value = scrollBar.Value;
+            UpdateColor();
+        }
+
+        private void Numeric_Red_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown numericUpDown = (NumericUpDown)sender;
+            ScrollBar scrollBar = (ScrollBar)numericUpDown.Tag;
+            scrollBar.Value = (int)numericUpDown.Value;
+        }
+
+        private void Numeric_Green_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown numericUpDown = (NumericUpDown)sender;
+            ScrollBar scrollBar = (ScrollBar)numericUpDown.Tag;
+            scrollBar.Value = (int)numericUpDown.Value;
+        }
+
+        private void Scroll_Blue_ValueChanged(object sender, ScrollEventArgs e)
+        {
+            ScrollBar scrollBar = (ScrollBar)sender;
+            NumericUpDown numericUpDown = (NumericUpDown)scrollBar.Tag;
+            numericUpDown.Value = scrollBar.Value;
+            UpdateColor();
+        }
+
+        private void Numeric_Blue_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown numericUpDown = (NumericUpDown)sender;
+            ScrollBar scrollBar = (ScrollBar)numericUpDown.Tag;
+            scrollBar.Value = (int)numericUpDown.Value;
+        }
         private void UpdateColor()
         {
-            colorResult = Color.FromArgb(hscrollBar_red.Value, hscrollBar_green.Value, hscrollBar_blue.Value);
+            colorResult = Color.FromArgb(Scroll_Red.Value, Scroll_Green.Value, Scroll_Blue.Value);
             pictureBox1.BackColor = colorResult;
         }
 
-        private void numericUpDown_red_ValueChanged(object sender, EventArgs e)
-        {
-            NumericUpDown numericUpDown = (NumericUpDown)sender;
-            ScrollBar scrollBar = (ScrollBar)numericUpDown.Tag;
-            scrollBar.Value = (int)numericUpDown.Value;
-            UpdateColor();
-        }
-
-        private void hscrollBar_green_Scroll(object sender, ScrollEventArgs e)
-        {
-            ScrollBar scrollBar = (ScrollBar)sender;
-            NumericUpDown numericUpDown = (NumericUpDown)scrollBar.Tag;
-            numericUpDown.Value = scrollBar.Value;
-            UpdateColor();
-        }
-
-        private void numericUpDown_green_ValueChanged(object sender, EventArgs e)
-        {
-            NumericUpDown numericUpDown = (NumericUpDown)sender;
-            ScrollBar scrollBar = (ScrollBar)numericUpDown.Tag;
-            scrollBar.Value = (int)numericUpDown.Value;
-            UpdateColor();
-        }
-
-        private void hscrollBar_blue_Scroll(object sender, ScrollEventArgs e)
-        {
-            ScrollBar scrollBar = (ScrollBar)sender;
-            NumericUpDown numericUpDown = (NumericUpDown)scrollBar.Tag;
-            numericUpDown.Value = scrollBar.Value;
-            UpdateColor();
-        }
-
-        private void numericUpDown_blue_ValueChanged(object sender, EventArgs e)
-        {
-            NumericUpDown numericUpDown = (NumericUpDown)sender;
-            ScrollBar scrollBar = (ScrollBar)numericUpDown.Tag;
-            scrollBar.Value = (int)numericUpDown.Value;
-            UpdateColor();
-        }
-
-        private void buttonOther_Click(object sender, EventArgs e)
-        {
-            ColorDialog colorDialog = new ColorDialog();
-            if (colorDialog.ShowDialog() == DialogResult.OK)
-            {
-                hscrollBar_red.Value = colorDialog.Color.R;//rgb цвета
-                hscrollBar_green.Value = colorDialog.Color.G;
-                hscrollBar_blue.Value = colorDialog.Color.B;
-
-                colorResult = colorDialog.Color;
-
-                UpdateColor();
-
-            }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void Button1_Click(object sender, EventArgs e)
         {
             Form1.currentPen.Color = colorResult;
             Close();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void Button2_Click(object sender, EventArgs e)
         {
             Close();
         }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                Scroll_Red.Value = colorDialog.Color.R;
+                Scroll_Green.Value = colorDialog.Color.G;
+                Scroll_Blue.Value = colorDialog.Color.B;
+
+                colorResult = colorDialog.Color;
+
+                UpdateColor();
+            }
+            Form1 Main = Owner as Form1;
+        }
     }
+
 }
